1813e74a8dffb76376178cc2fe0519f9a08b63cc
Igor Aguiar <80079216+IgorAguiar-Giro@users.noreply.github.com>
2024-01-04 14:40:07 +0000

    add tests to database

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt); //check if date is ISO8601
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0"); //hardcoded test version for Postgres Version
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100); //hardcoded test for max connections
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1); //Test for Opened Connections
 });
