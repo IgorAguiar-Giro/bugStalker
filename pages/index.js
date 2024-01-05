4c0929d6059e9cd3b5971936437eb9c1072646c9
Igor Aguiar <80079216+IgorAguiar-Giro@users.noreply.github.com>
2024-01-05 21:37:49 +0000

    add `Navbar.js` and `Button.js` to `.\components`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import Navbar from "components/Navbar";
+
 export default function Page() {
-  return <p className='myclass'>Some text</p>
+  return (<>
+  <Navbar />
+    <div className="bg-indigo-600 w-full h-screen"></div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </>);
 }
